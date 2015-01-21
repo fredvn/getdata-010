35d10f33e336a15c63cffdd25ce0109e881507f3
fredvn <fred.van.nimwegen@gmail.com>
2015-01-21 14:15:14 +0100

    Codebook

--- run_analysis.R
+++ run_analysis.R
@@ -23,6 +23,13 @@ run_analysis <- function( directory = "UCI HAR Dataset", tidyFile = "tidy.txt") 
  ##
  c <- read.table(paste(directory,"/features.txt", sep=""),header=FALSE,sep = " ")
  columns <- c("subject","activity","activityDescription",as.vector(c$V2))
+ cleanColumns <- gsub("[[:punct:]]","",columns)
+ for ( i in 1:length(cleanColumns) ){
+ print(cleanColumns[i])
+    if (sum(cleanColumns == cleanColumns[i]) > 1 ) {
+	   cleanColumns[i] = i
+    }
+ }
  
  #
  # Activity matrix
@@ -32,16 +39,18 @@ run_analysis <- function( directory = "UCI HAR Dataset", tidyFile = "tidy.txt") 
  #
  # Load and combine test dataset
  #
- testset <- dataset(directory,set="test",columns)
+ testset <- dataset(directory,set="test",cleanColumns)
  #
  # Load and combine train dataset
  #
- trainset <- dataset(directory,set="train",columns)
+ trainset <- dataset(directory,set="train",cleanColumns)
  
- tidyset <- rbind(testset,trainset)
+ totalset <- rbind(testset,trainset)
  
- write.table(tidyset,tidyFile, row.name=FALSE)
+ subset <- select(totalset,matches("(Subject|Activity|mean|std)",ignore.case = TRUE))
+ result <- subset %>% group_by(subject,activity,activityDescription) %>% summarise_each(funs(mean))
+ write.table(result,tidyFile, row.name=FALSE)
  
- tidyset
+ result
  
 }
